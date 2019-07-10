28b4a90c6ccf54199d355eb6babcadc7f0a96cc7
Mary West <westmarye1219@gmail.com>
2019-07-09 21:21:26 -0500

    read complete

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -30,9 +30,11 @@ class Home extends React.Component {
     ));
 
     return (
-      <div className="Home">
+      <div className="Home col-4">
         <h1>Home</h1>
+        <div className = "d-flex">
         {makeStuffCards}
+        </div>
       </div>
     );
   }
